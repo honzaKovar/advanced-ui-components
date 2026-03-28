7c0f97a2e126edb29baa8e3ae9905fc9473733c2
Honza Kovář <jan.kovar@chyron.com>
2026-03-28 10:44:30 +0100

    Updates

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+export default {
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-essentials"],
+};
