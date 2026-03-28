d0f427ad471738de76ea6c585fd3bbba7510ea8a
Honza Kovář <jan.kovar@chyron.com>
2026-03-28 10:21:40 +0100

    chore: initial setup with configurations and packages

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const config: JestConfigWithTsJest = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testMatch: ["**/*.spec.ts", "**/*.spec.tsx", "**/*.test.ts", "**/*.test.tsx"],
+};
+
+export default config;
