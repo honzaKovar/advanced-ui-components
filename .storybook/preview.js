1fed1dc6bc500fb95617ae6d7034c12bf7a1a1d7
Honza Kovář <jan.kovar@chyron.com>
2026-03-28 16:02:53 +0100

    fix: simplifying config for failing deployed story

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,25 +2,23 @@ export const parameters = {};
 
 export const decorators = [
   (Story) => {
-    if (typeof document !== "undefined") {
-      const observer = new MutationObserver((mutations) => {
-        for (const mutation of mutations) {
-          mutation.addedNodes.forEach((node) => {
-            if (
-              node.nodeType === 1 &&
-              node.textContent?.includes("MUI X Missing license key")
-            ) {
-              node.lastChild?.remove();
-            }
-          });
-        }
-      });
+    const observer = new MutationObserver((mutations) => {
+      for (const mutation of mutations) {
+        mutation.addedNodes.forEach((node) => {
+          if (
+            node.nodeType === 1 &&
+            node.textContent?.includes("MUI X Missing license key")
+          ) {
+            node.lastChild.remove();
+          }
+        });
+      }
+    });
 
-      observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-      });
-    }
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
 
     return Story();
   },
